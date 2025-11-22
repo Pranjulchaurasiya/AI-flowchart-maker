c62a30f35353a5350ab9981acd2f92fc23b2a5f2
Pranjul Chaurasiya <153517820+Pranjulchaurasiya@users.noreply.github.com>
2025-11-23 01:23:25 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-const API_BASE_URL = 'import.meta.env.https://ai-flowchart-maker.onrender.com'
+const API_BASE_URL = 'const API_BASE_URL = import.meta.env.VITE_API_URL;
+'
 
 export const generateFlowchart = async (input, mode, orientation = 'horizontal') => {
   try {
